b1c21a686019cfd86563247e8e9e47b63f787396
gautamgit676 <gautamgit244@gmail.com>
2026-06-29 18:45:33 +0530

    add db server

--- pro/pro/settings.py
+++ pro/pro/settings.py
@@ -73,12 +73,7 @@ WSGI_APPLICATION = 'pro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -94,10 +89,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-
-
-
 
 
 # Password validation
