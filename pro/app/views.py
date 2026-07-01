6d63da615cd96cb27ca9cba5430a88af41285936
gautamgit676 <gautamgit244@gmail.com>
2026-07-01 15:37:39 +0530

    add limit off photo 5 mb

--- pro/app/views.py
+++ pro/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect
 from .models import Photo
+from django.contrib import messages
+
 # Create your views here.
 
 
@@ -11,21 +13,33 @@ def  home(request):
 def  basedemo(request):
     return render(request , 'base.html')
 
-
-
 def upload_photo(request):
 
     if request.method == "POST":
         title = request.POST.get("title")
         image = request.FILES.get("image")
 
+        # ❌ 1. Check empty upload
+        if not image:
+            messages.error(request, "Please select an image")
+            return redirect("upload")
+
+        # ❌ 2. File size limit (5MB example)
+        if image.size > 5 * 1024 * 1024:
+            messages.error(request, "Image must be under 5MB")
+            return redirect("upload")
+
+        
+
+        # ✅ Save safely
         Photo.objects.create(
             title=title,
             image=image
         )
 
+        messages.success(request, "Photo uploaded successfully!")
         return redirect("upload")
 
     photos = Photo.objects.all()
+    return render(request, "pics.html", {"photos": photos})
 
-    return render(request,"pics.html", {"photos": photos})
